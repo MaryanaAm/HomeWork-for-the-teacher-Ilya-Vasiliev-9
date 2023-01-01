51d4703865dd82ffbe8ca434ae928357cf5e411d
MaryanaAm <marydeaddoll@gmail.com>
2023-01-01 10:38:54 +0300

    add files

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NumberList(int number)
+{
+    if(number == 0) return;
+        Console.Write(number + " ");
+        NumberList(number - 1);
+            
+}
+
+NumberList(number);
+
